8be65c2b0bd9b6a69b131b31a716fca66614666e
selvakumaranram <skumaran449@gmil.com>
2024-04-02 11:16:15 +0530

    adding function for adding string number

--- src/main/java/org/craftsMansTDD/StringCalculator.java
+++ src/main/java/org/craftsMansTDD/StringCalculator.java
@@ -2,6 +2,22 @@ package org.craftsMansTDD;
 
 public class StringCalculator {
     public int addStringNumbers(String numbers) {
-        return 0;
+        int sum = 0;
+        // Split the string based on the following special characters
+        String[] numsArray = numbers.split("[,;/\\n//]");
+        // Iterate through numsArray array
+        for (String num : numsArray) {
+            try {
+                int number = Integer.parseInt(num);
+                if (number > 0) {
+                    sum += number;
+                }
+
+            } catch (NumberFormatException ignored) {
+                // Ignore non-numeric parts
+            }
+        }
+        return sum;
+
     }
 }
